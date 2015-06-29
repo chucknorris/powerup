793cbf3b9ad7547ae798700e100aaeba3e968d42
Steven Liekens <steven.liekens@gmail.com>
2015-06-29 14:21:06 +0200

    Changing templates to use brackets and unicode strings
    This commit also fixes the maximum length for database object
    names: from 100 to 128.
    
    Fixes #22

--- src/Powerup/Templates/DropCreateTemplate.cs
+++ src/Powerup/Templates/DropCreateTemplate.cs
@@ -4,30 +4,27 @@ namespace Powerup.Templates
 {
     public class DropCreateTemplate : TemplateBase
     {
-        /// <summary>
-        /// 0: Name
-        /// 1: Schema
-        /// 2: Code
-        /// 3: Type (PROCEDURE, VIEW, etc)
-        /// 4: Database
-        /// </summary>
-        const string Template =
-            @"IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'{1}.{0}')) DROP {3} {1}.{0}
-GO
-
-{2} ";
-
         public DropCreateTemplate(SqlObject sqlObject) : base(sqlObject)
         {
         }
 
         public override string TemplatedProcedure()
         {
-            return string.Format(Template,
-                                    _sqlObject.Name,
-                                    _sqlObject.Schema,
-                                    Proc,
-                                    _sqlObject.SqlType.ToString().ToUpper());
+            return string.Format(
+                @"DECLARE @Name nvarchar(128), @Type nvarchar(20), @Schema nvarchar(128)
+SELECT @Name = N'{0}', @Type = N'{1}', @Schema = N'{2}'
+
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID('[' + @Schema + '].[' + @Name + ']'))  
+    EXECUTE('DROP ' + @Type + ' [' + @Schema + '].[' + @Name + ']')
+
+PRINT 'Creating ' + @Type + ' [' + @Schema + '].[' + @Name + ']'
+GO
+
+{3}",
+                this._sqlObject.Name,
+                this._sqlObject.SqlType.ToString().ToUpper(),
+                this._sqlObject.Schema,
+                this.Proc);
         }
 
         public override void AddText(string text)
